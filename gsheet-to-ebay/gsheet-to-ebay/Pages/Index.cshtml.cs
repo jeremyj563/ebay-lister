2b760a56154ca5f35e3e66856db05bfe842673a6
Jeremy Johnson <jeremyj@pcdomain.com>
2018-02-28 08:23:29 -0600

    first commit

---
+++ gsheet-to-ebay/gsheet-to-ebay/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gsheet_to_ebay.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
